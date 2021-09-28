b31002d9c66295de0722f8cf5ded330f049e786e
nivit17 <nivedita.t@sodelsolutions.com>
2021-09-28 18:23:11 +0530

    Sample

---
+++ first.rb
@@ -0,0 +1,23 @@
+puts "hello world"
+print "hello"
+
+BEGIN{
+  puts "start"
+}
+
+END{
+ puts "end"
+}
+
+def sample
+  puts "\nsample test run"
+  i=20
+end
+
+puts sample
+
+puts "adajdj@\b$dsfkjsdlfk"
+
+things_i_like = "ice cream, chocolate, movies, beaches"
+
+
